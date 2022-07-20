441e4b85fbc5cdfd51ac79fefc542f80abc9ecd6
Rajesh dalai <rdalai@vertel.com.au>
2022-07-20 15:54:50 +0530

    new changes

--- src/model/validation/valiDator.ts
+++ src/model/validation/valiDator.ts
@@ -4,10 +4,10 @@ import mongoose from "mongoose";
 const mobileRegex:RegExp= /^[6-9]{1}[0-9]{9}$/
 
 const userSchema = joi.object({
-    name:joi.string().min(3).max(30).uppercase(),
+    name:joi.string().uppercase().min(3).max(30),
     email: joi.string().email().min(3).max(200).lowercase(),
     phone:joi.string().pattern(new RegExp('^[[6-9]{1}[0-9]{9}$')),
-    designation:joi.string().min(3).max(20).uppercase()
+    designation:joi.string().min(3).max(200).uppercase()
 }) 
 
 export default userSchema;
